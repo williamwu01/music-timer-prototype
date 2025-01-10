7f454e1e5cce58dc8dda39b7324969aba1ef9bab
William Wu <williamwuu3@gmail.com>
2025-01-10 15:37:18 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+
+MP3_PATH = ""
+VLC_PATH = ""
+
+def play_music_on_vlc():
+		pass
+
+#for testing 
+# Get the current working directory
+current_dir = os.getcwd()
+print("Current Directory:", current_dir)
+
+# List files and directories in the current directory
+files = os.listdir()
+print("Files in directory:", files)
+
+# Create a new directory
+# os.mkdir("new_directory")
+# print("New directory created!")
+
