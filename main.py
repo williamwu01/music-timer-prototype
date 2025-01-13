0b1dec449682962ff02e26cef40eef146c3d15e5
unknown <williamwuu3@gmail.com>
2025-01-13 14:44:36 -0800

    tested the function

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import os
 
-MP3_PATH = ""
-VLC_PATH = ""
-
+MP3_PATH = "C:\\Users\\info\\Downloads\\test.mp3"
+    
 def play_music_on_vlc():
-		pass
+	## function for playing the music on vlc 
+    try:
+        print("Playing MP3 file with VLC...")
+        os.system("vlc " + " --intf dummy " + MP3_PATH + ' --play-and-exit')
+    except Exception as e:
+        print(f"Error while trying to play MP3: {e}")
+
+def main():
+    play_music_on_vlc()
+
+main()
+
+
 
 #for testing 
 # Get the current working directory
-current_dir = os.getcwd()
-print("Current Directory:", current_dir)
+# current_dir = os.getcwd()
+# print("Current Directory:", current_dir)
 
 # List files and directories in the current directory
-files = os.listdir()
-print("Files in directory:", files)
+# files = os.listdir()
+# print("Files in directory:", files)
 
 # Create a new directory
 # os.mkdir("new_directory")
 # print("New directory created!")
-
